64e638c0a1dce128d8a685b33c15788c33f4d08b
James Novak <jamesnovak90@gmail.com>
2016-09-25 18:47:19 -0400

    setup API, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do 
-      resources :[model_name_pluralized]
+      resources :providers
     end
   end
 end
