13d69fe3dbf7166cb548dd9540c4075e755d54f5
James Novak <jamesnovak90@gmail.com>
2016-09-25 22:16:28 -0400

    configure angular routes

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,2 +1,32 @@
 var app = angular
-  .module('app', ['ui.router', 'ngResource']) 
+  .module('app', ['ui.router', 'ngResource'])
+  .config(function ($stateProvider, $urlRouterProvider){
+   $stateProvider
+     .state('home', {
+       url: '/',
+       templateUrl: 'home.html',
+       controller: 'HomeController as ctrl'
+     })
+     .state('home.about', {
+       url: 'about',
+       templateUrl: 'home/about.html',
+       controller: 'HomeController as ctrl'
+     })
+     .state('home.new', {
+       url: 'new',
+       templateUrl: 'home/new.html',
+       controller: 'ProvidersController as ctrl'
+     })
+    .state('home.providers', {
+       url: 'providers',
+       templateUrl: 'home/providers.html',
+       controller: 'ProvidersController as ctrl'
+     })
+     .state('home.provider', {
+       url: 'provider/:id',
+       templateUrl: 'home/show.html',
+       controller: 'ProvidersController as ctrl'
+     })
+
+  $urlRouterProvider.otherwise('/');
+}); 
