83a6b4559e27c319af2c56d74a78d02a65967e78
James Novak <jamesnovak90@gmail.com>
2016-09-28 02:05:28 -0400

    working ng-click for search button, displaying provider results

--- app/assets/javascripts/angular/controllers/HomeController.js
+++ app/assets/javascripts/angular/controllers/HomeController.js
@@ -1,14 +1,15 @@
 app.controller('HomeController', function(ProviderService, $location){
   var ctrl = this
+  var url = 'http://localhost:3000/api/v1/providers'
+
   this.showHome = function(){
     return $location.path() === '/'
   } 
 
-  this.searchApp = function(){
-    var results = []
-    ProviderService.getProviders()
-    .then(function(response) {
-      results.push(response.data)
+  this.search = function(){
+    ProviderService.getProviders().success(function(data){
+      ctrl.items = data
     })
   }
+
 })
