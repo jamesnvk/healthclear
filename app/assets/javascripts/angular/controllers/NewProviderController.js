3b0f436c3269f66d7671e7d56988a78b6b285144
James Novak <jamesnovak90@gmail.com>
2016-09-27 22:32:45 -0400

    user can create provider through form and provider service post request

--- app/assets/javascripts/angular/controllers/NewProviderController.js
+++ app/assets/javascripts/angular/controllers/NewProviderController.js
@@ -1,6 +1,9 @@
 app.controller('NewProviderController', function(ProviderService, $location){
   var ctrl = this
   ctrl.addProvider = function() {
-    debugger
+    //debugger
+    var data = {name: this.provider.name}
+    ProviderService.createProvider(data)
+    $location.path('providers')
   }
 })
--- app/assets/javascripts/angular/services/ProviderService.js
+++ app/assets/javascripts/angular/services/ProviderService.js
@@ -1,5 +1,8 @@
 app.service('ProviderService', function($http){
   this.getProviders = function() {
     return $http.get('http://localhost:3000/api/v1/providers');
-  };
+  }
+  this.createProvider = function(data){
+    return $http.post('http://localhost:3000/api/v1/providers', data)
+  }
 })
