bd2efb9d52b00f713a7753eecf41e491305f4308
James Novak <jamesnovak90@gmail.com>
2016-09-28 01:19:14 -0400

    building search

--- app/assets/javascripts/angular/controllers/HomeController.js
+++ app/assets/javascripts/angular/controllers/HomeController.js
@@ -1,6 +1,14 @@
-app.controller('HomeController', function($location){
+app.controller('HomeController', function(ProviderService, $location){
   var ctrl = this
   this.showHome = function(){
     return $location.path() === '/'
   } 
+
+  this.searchApp = function(){
+    var results = []
+    ProviderService.getProviders()
+    .then(function(response) {
+      results.push(response.data)
+    })
+  }
 })
