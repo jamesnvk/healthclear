1e8943ed988ac7c7daad898a828d33de74aef3f8
James Novak <jamesnovak90@gmail.com>
2016-10-01 21:50:27 -0400

    add forms for price, radio buttons giving back null property for price

--- app/assets/javascripts/angular/controllers/NewProviderController.js
+++ app/assets/javascripts/angular/controllers/NewProviderController.js
@@ -2,7 +2,8 @@ app.controller('NewProviderController', function(ProviderService, $location){
   var ctrl = this
   ctrl.addProvider = function() {
     //debugger
-    var data = {name: this.provider.name}
+    var data = {name: this.provider.name, price: this.provider.price}
+    debugger
     ProviderService.createProvider(data)
     $location.path('providers')
   }
